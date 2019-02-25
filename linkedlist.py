6db6edbfc0b9173996f88b927cc34d5e7be088e4
oguzhan <oguzhancelikarslan@gmail.com>
2019-02-25 23:12:08 +0300

    Başından node ekleme fonksyionu eklendi

--- linkedlist.py
+++ linkedlist.py
@@ -20,6 +20,13 @@ class LinkedList():
             print(temp.value)
             temp = temp.nextNode
 
+    # Yeni node'u başından ekleme fonksiyonu
+    def AtBegining(self,value):
+        newNode = Node(value)
+        # Oluşturduğumuz yeni node değerinin pointerı eski header olmalıdır
+        newNode.nextNode = self.header
+        # Artık baştan yeni eleman eklediğimiz için header yeni node olmalıdır.
+        self.header = newNode
 
 # Bağlı listemizi oluştruduk.
 listLinked = LinkedList()
@@ -36,5 +43,7 @@ listLinked.header.nextNode = e2
 e2.nextNode = e3
 # E3'ün sonu ise None olduğu için bir yere bağlama gereksinimi duymadık bu bizim için tailor oldu.
 
+listLinked.AtBegining("oğuzhan")
+listLinked.AtBegining("Sefa")
 listLinked.printAllValuesOfNodes()
 
