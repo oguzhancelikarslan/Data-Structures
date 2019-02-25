adb935113388f149dfb77df91994729a508c4ecd
oguzhan <oguzhancelikarslan@gmail.com>
2019-02-25 23:39:09 +0300

    Listenin sonundan ekleme işlemi

--- linkedlist.py
+++ linkedlist.py
@@ -28,6 +28,19 @@ class LinkedList():
         # Artık baştan yeni eleman eklediğimiz için header yeni node olmalıdır.
         self.header = newNode
 
+    def AtEnd(self, value):
+        # yeni node'u oluşturalım
+        newNode = Node(value)
+        # ilk öncelikle en son nodeu ilk node olarak kabul ediyoruz..
+        last_node = self.header
+        while last_node.nextNode is not None:
+            last_node = last_node.nextNode
+        # Bu while döngüsü sonucunda, en sonuncu nodedan bir önceki node u bulduk
+        # daha sonra ise sondan bir önceki nodeun nextini kullanarak yani son  nodeu bularak
+        # onun nextini new Node yapıyoruz. Daha türkçesi son nodeun işaret ettiği değer yeni değerimiz oluyor.
+        last_node.nextNode = newNode
+
+
 # Bağlı listemizi oluştruduk.
 listLinked = LinkedList()
 
@@ -43,7 +56,12 @@ listLinked.header.nextNode = e2
 e2.nextNode = e3
 # E3'ün sonu ise None olduğu için bir yere bağlama gereksinimi duymadık bu bizim için tailor oldu.
 
-listLinked.AtBegining("oğuzhan")
+listLinked.AtBegining("Oğuzhan")
 listLinked.AtBegining("Sefa")
+
+# Sondan listeye eleman ekleyelim
+listLinked.AtEnd("Enes")
+listLinked.AtEnd("Harun")
+listLinked.AtEnd("Orhan")
 listLinked.printAllValuesOfNodes()
 
